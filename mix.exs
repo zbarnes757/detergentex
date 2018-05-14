7a9d39d4a48e1f11bf2148dc9a7681e5aee2a8a8
Zac <zbarnes757@users.noreply.github.com>
2018-05-14 14:22:25 -0500

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule Detergentex.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:detergent, "~> 0.3.0" },
+      {:detergent, github: "devinus/detergent"},
       {:erlsom, github: "willemdj/erlsom"}
     ]
   end
